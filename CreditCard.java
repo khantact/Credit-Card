c64bb9688d0827607d91adfae0ede8f27cc6e51f
Kevin Han <khan@colgate.edu>
2022-02-27 23:44:04 -0500

    Missing 1 update to toString

--- CreditCard.java
+++ CreditCard.java
@@ -1,11 +1,10 @@
 import java.util.Calendar;
 import java.util.Random;
 
+public class CreditCard {
 
-public class CreditCard{
+     // Variable Declaration //
 
-
-     //             Variable Declaration             //
      private static final Random rand = new Random();
      private String accountName = new String();
      private int expirationMonth = 0;
@@ -13,114 +12,193 @@ public class CreditCard{
      private double creditLimit = 0.0;
      private double balance = 0.0;
      private int cvv = 0;
-     private static int accountNumber = 1999;
+     private static int count = 2000;
+     private int accountNumber = 0;
 
+     // Constructors //
 
-
-     public CreditCard(String accountName, int expirationMonth, int expirationYear, double creditLimit){
+     public CreditCard(String accountName, int expirationMonth, int expirationYear, double creditLimit) {
           this.accountName = accountName;
           this.creditLimit = 400;
-          cvv = rand.nextInt(1000);
-          accountNumber++;
-          if (creditLimit > 0.0){
+          cvv = rand.nextInt(900) + 100;
+          this.accountNumber = count++;
+          if (creditLimit > 0.0) {
                this.creditLimit = creditLimit;
           }
 
-          if (dateCheck(expirationMonth, expirationYear)){
+          if (dateCheck(expirationMonth, expirationYear)) {
                this.expirationMonth = expirationMonth;
                this.expirationYear = expirationYear;
-          }
-          else{
+          } else {
                this.expirationMonth = getCurrentMonth();
                this.expirationYear = getCurrentYear();
           }
      }
 
-     public CreditCard(String accountName, int expirationMonth, int expirationYear){ 
-          this(accountName,expirationMonth,expirationYear,400);
+     public CreditCard(String accountName, int expirationMonth, int expirationYear) {
+          this(accountName, expirationMonth, expirationYear, 400);
      }
 
-     public CreditCard(String accountName){ 
-          this(accountName,getCurrentMonth(),getCurrentYear()+3,400);
+     public CreditCard(String accountName) {
+          this(accountName, getCurrentMonth(), getCurrentYear() + 3, 400);
      }
 
-     public String toString(){
-          return accountName+"("+accountNumber+"): Current Balance = $"+balance+"/"+creditLimit+", Expires: "+expirationMonth +"/"+expirationYear;
+     public String toString() {
+          return accountName + "(" + accountNumber + "): Current Balance = $" + balance + "/" + creditLimit
+                    + ", Expires: " + expirationMonth + "/" + expirationYear;
      }
 
-     //             Accessors             //      
+     // Accessors //
 
-     public String getName(){
+     public String getName() {
           return accountName;
      }
 
-     public double getCreditLimit(){
+     public double getCreditLimit() {
           return creditLimit;
      }
 
-     public int getExpireMonth(){
+     public int getExpireMonth() {
           return expirationMonth;
      }
 
-     public int getExpireYear(){
+     public int getExpireYear() {
           return expirationYear;
      }
 
-     public double getBalance(){
+     public double getBalance() {
           return balance;
      }
-     
-     public boolean chargeCard(double amount, int cvv){
-          if (getBalance() + amount > getCreditLimit()){
+
+     // Hard Coding Tests - for testing only //
+
+     public void setCvv(int cvv) {
+          this.cvv = cvv;
+     }
+
+     public void getCvv() {
+          System.out.println(this.cvv);
+     }
+
+     // Object Methods //
+
+     public boolean chargeCard(double amount, int cvv) {
+          if (getBalance() + amount > getCreditLimit() || cvv != this.cvv || amount < 0) {
                return false;
           }
-          //if (cvv != )
+
+          balance += amount;
           return true;
      }
 
-     public boolean payCard(double amount){
-          if (getBalance() - amount < 0 || amount < 0){
+     public boolean payCard(double amount) {
+          if (amount < 0 || getBalance() < 0) {
                return false;
           }
+          balance -= amount;
           return true;
      }
 
-     private static int retrieveCVV(CreditCard card, String name, int accountNumber){
-          if (card.getName().equals(name) && card.accountNumber == (accountNumber)){
-               // return thiscvv;
+     public void extendExpireDate(int months) {
+          int years = months / 12;
+          int remaining_months = months % 12;
+          if (months < 0) {
+               return;
+          }
+          expirationMonth += remaining_months;
+          expirationYear += years;
+
+     }
+
+     public static void extendOnlyExpired(CreditCard[] cards, int months) {
+          if (months < 0) {
+               return;
+          }
+          for (int i = 0; i < cards.length; i++) { // iterates through credit card array
+               if (cards[i].dateCheck(cards[i].getExpireMonth(), cards[i].getExpireYear())) {
+                    if (cards[i].getExpireYear() < getCurrentYear()) {
+                         cards[i].extendExpireDate(months);
+                    } else if (cards[i].getExpireYear() == getCurrentYear()) {
+                         if (cards[i].getExpireMonth() < getCurrentMonth()) {
+                              cards[i].extendExpireDate(months);
+                         }
+                    }
+               }
+          }
+     }
+
+     public static int retrieveCvv(CreditCard card, String name, int accountNum) {
+          if (card.getName().equals(name) && card.accountNumber == (accountNum)) {
+               return card.cvv;
           }
           return -1;
      }
 
-     private boolean dateCheck(int expirationMonth, int expirationYear){ 
-          if (expirationYear > getCurrentYear()+10 || expirationYear < getCurrentYear()-10){
+     public static CreditCard mergeCards(CreditCard c1, CreditCard c2) {
+          double newCreditLimit = c1.getCreditLimit() + c2.getCreditLimit();
+          double newBalance = c1.getBalance() + c2.getBalance();
+          String newName = c1.getName() + " and " + c2.getName();
+          int newExpirationMonth = 0;
+          int newExpirationYear = 0;
+
+          if (c1.getExpireMonth() > c2.getExpireMonth()) {
+               newExpirationMonth = c1.getExpireMonth();
+          } else {
+               newExpirationMonth = c2.getExpireMonth();
+          }
+
+          if (c1.getExpireYear() > c2.getExpireYear()) {
+               newExpirationYear = c1.getExpireYear();
+          } else {
+               newExpirationYear = c2.getExpireYear();
+          }
+          destroyCards(c1, c2);
+          CreditCard newCard = new CreditCard(newName, newExpirationMonth, newExpirationYear, newCreditLimit);
+          newCard.balance = newBalance;
+          return newCard;
+     }
+
+     // Helper Functions //
+
+     private static void destroyCards(CreditCard c1, CreditCard c2) {
+          c1.balance = -1;
+          c1.cvv = -1;
+          c1.expirationMonth = -1;
+          c1.expirationYear = -1;
+          c1.creditLimit = -1;
+          c1.accountName = "Merged with #" + c2.accountNumber;
+
+          c2.balance = -1;
+          c2.cvv = -1;
+          c2.expirationMonth = -1;
+          c2.expirationYear = -1;
+          c2.creditLimit = -1;
+          c2.accountName = "Merged with #" + c1.accountNumber;
+
+     }
+
+     private boolean dateCheck(int expirationMonth, int expirationYear) {
+          if (expirationYear > getCurrentYear() + 10 || expirationYear < getCurrentYear() - 10) {
                return false;
           }
 
-          if (expirationMonth > 12 || expirationMonth < 1){
+          if (expirationMonth > 12 || expirationMonth < 1) {
                return false;
           }
           return true;
      }
 
-
-
-
-     //***   Try us!  See what these methods' returned values look like!  ****
-     public static int getCurrentMonth(){
+     public static int getCurrentMonth() {
           Calendar currentDate = Calendar.getInstance();
           return currentDate.get(currentDate.MONTH) + 1;
 
      }
 
-     public static int getCurrentYear(){
+     public static int getCurrentYear() {
           Calendar currentDate = Calendar.getInstance();
           return currentDate.get(currentDate.YEAR);
 
      }
-    //************************************************************************
-
-
-
+     // ************************************************************************
 
 }
--- CreditCardClient.java
+++ CreditCardClient.java
@@ -1,38 +1,86 @@
 public class CreditCardClient {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        // CreditCard khan = new CreditCard("Kevin Han", 6, 2020, 500);
+        // CreditCard khan1 = new CreditCard("Kevin Han", 4, 2024, 700);
+        // System.out.println(khan);
+        // System.out.println(khan1);
+        // Test constructor redirection //
+        /*
+         * CreditCard c1 = new CreditCard("Kevin Han");
+         * CreditCard c1 = new CreditCard("Kevin Han", 4, 2024);
+         * CreditCard c1 = new CreditCard("Kevin Jam", 6, 2025, 450);
+         */
+        // Test creditLimit //
+        /*
+         * CreditCard c1 = new CreditCard("Kevin Han",4,2024,-300);
+         * CreditCard c1 = new CreditCard("Kevin Han",14,2024,400);
+         * CreditCard c1 = new CreditCard("Kevin Han",14,2024,0);
+         */
+        // Test expirationYear/Month //
+        /*
+         * CreditCard c1 = new CreditCard("Kevin Han", 4, 2100);
+         * CreditCard c1 = new CreditCard("Kevin Han", 0, 2025);
+         * CreditCard c1 = new CreditCard("Kevin Han", 4, 1345);
+         */
 
-        //                 Constructor testing                 //
+        // Test counter increment //
+        /*
+         * CreditCard c1 = new CreditCard("Kevin Han", 4, 2024, 150);
+         * CreditCard c2 = new CreditCard("Kevin Jam", 7, 2025, 450);
+         * System.out.println(c1);
+         * System.out.println(c2);
+         */
 
-            //          Test constructor redirection          //
-                //CreditCard c1 = new CreditCard("Kevin Han");
-                //CreditCard c1 = new CreditCard("Kevin Han", 4, 2024);
-                //CreditCard c1 = new CreditCard("Kevin Jam", 6, 2025, 450);
-            
-            //          Test creditLimit          //
-                //CreditCard c1 = new CreditCard("Kevin Han",4,2024,-300);
-                //CreditCard c1 = new CreditCard("Kevin Han",14,2024,400);
-                // CreditCard c1 = new CreditCard("Kevin Han",14,2024,0);
+        // Test extendOnlyExpired //
+        /*
+         * CreditCard[] cards = { khan, khan1 };
+         * CreditCard.extendOnlyExpired(cards, 2);
+         * CreditCard.extendOnlyExpired(cards, 14);
+         * CreditCard.extendOnlyExpired(cards, -1);
+         */
+        // Test extendExpireDate //
+        /*
+         * khan.extendExpireDate(14);
+         * khan.extendExpireDate(-1);
+         */
 
-            //          Test expirationYear/Month          //
-                // CreditCard c1 = new CreditCard("Kevin Han", 4, 2100);
-                // CreditCard c1 = new CreditCard("Kevin Han", 0, 2025);
-                // CreditCard c1 = new CreditCard("Kevin Han", 4, 1345);
-
-            //          Test counter increment      //
-                // CreditCard c1 = new CreditCard("Kevin Han", 4, 2024, 150);
-                // CreditCard c2 = new CreditCard("Kevin Jam", 7, 2025, 450);
-            //System.out.println(c1);
-            //System.out.println(c2);
-        //                         End of Testing                         //
- 
-        CreditCard khan = new CreditCard("Kevin Han", 4, 2024, 500);
+        // Test chargeCard & payCard //
+        /*
+         * khan.setCvv(124);
+         * khan.chargeCard(125.34, 124);
+         * khan.chargeCard(-24, 124);
+         * khan.chargeCard(125.34, 100);
+         * khan.payCard(245);
+         * khan.payCard(124.50);
+         * khan.payCard(-40);
+         * khan.payCard(400);
+         * khan.payCard(245);
+         * khan.payCard(0);
+         * khan.chargeCard(250, 124);
+         */
+        // Test retrieveCvv //
+        /*
+         * khan.setCvv(124);
+         * System.out.println(CreditCard.retrieveCvv(khan, "Kevin Han", 2000));
+         * System.out.println(CreditCard.retrieveCvv(khan, "bob", 2000));
+         * System.out.println(CreditCard.retrieveCvv(khan, "Kevin Han", 2401));
+         */
+        // Test mergeCards //
+        /*
+        */
+        // CreditCard khan = new CreditCard("Kevin Han", 6, 2020, 500);
+        CreditCard khan = new CreditCard("Kevin Han", 6, 2020, 200);
         CreditCard khan1 = new CreditCard("Jane Doe", 4, 2024, 500);
+        khan1.chargeCard(130, 125);
+
+        khan.setCvv(124);
+        khan1.setCvv(125);
+        khan.chargeCard(120, 124);
+        khan1.payCard(400);
+
+        System.out.println(CreditCard.mergeCards(khan, khan1));
         System.out.println(khan);
         System.out.println(khan1);
-
-
-
-
 
     }
 }
